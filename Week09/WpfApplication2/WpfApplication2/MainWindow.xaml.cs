f123b8a9b851b477965ff17c050a02039b901dc7
woollybears <reaserkaitlyn@gmail.com>
2019-04-10 19:31:03 -0400

    FXDGTHD

---
+++ Week09/WpfApplication2/WpfApplication2/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using System.Windows.Input;
+
+
+namespace WpfApplication2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private EllipseGeometry myEllipseGeometry;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            Path myPath = new Path();
+            myPath.Fill = Brushes.IndianRed;
+            myPath.Data = myEllipseGeometry;
+            myCanvas.Children.Add(myPath);
+
+            myEllipseGeometry = new EllipseGeometry();
+            myEllipseGeometry.Center = new Point(200, 200);
+            myEllipseGeometry.RadiusX = 20;
+            myEllipseGeometry.RadiusY = 20;
+
+            Path myDog = new Path();
+            myPath.Fill = Brushes.IndianRed;
+            myPath.Data = myEllipseGeometry;
+
+            myCanvas.Children.Add(myDog);
+           
+
+
+        }
+
+
+
+        private void myCanvas_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            PointAnimation myPointAnimation = new PointAnimation(e.GetPosition(this), TimeSpan.FromSeconds(666));
+            myEllipseGeometry.BeginAnimation(EllipseGeometry.CenterProperty, myPointAnimation);
+        }
+    }
+}
