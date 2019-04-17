19fd58b212a68b8ffaa17961b14535dd7bcf236e
woollybears <reaserkaitlyn@gmail.com>
2019-04-17 19:32:19 -0400

    1321553

---
+++ WpfApplication4/WpfApplication4/Class1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WoollyBearsDataBase
+{
+    public class Woolly
+    {
+        string woollyName;
+        string woollyColor;
+        private int woollyLength;
+        private string woollyType;
+        private string ownerName;
+
+
+        public Woolly(string name, string color, string type, int length, string oname)
+        {
+            this.woollyName = name;
+            this.woollyColor = color;
+            this.woollyType = type;
+            this.woollyLength = length;
+            this.ownerName = oname;
+        }
+
+        public void ShowWoolly()
+        {
+            MessageBox.Show(woollyName + Environment.NewLine + woollyColor + Environment.NewLine + woollyType + Environment.NewLine + ownerName);
+        }
+    }
+
+    }
---
+++ WpfApplication4/WpfApplication4/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WoollyBearsDataBase
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public List<Woolly> WoollyBears;
+        public MainWindow()
+        {
+
+            WoollyBears = new List<Woolly>();
+
+        }
+        private void buttonWoolly_Click(object sender, RoutedEventArgs e)
+        {
+            string oname = onamebox.Text;
+            string name = namebox.Text;
+            string type = typebox.Text;
+            int length = Convert.ToInt32(lengthbox.Text);
+            string color = colorbox.Text;
+            Woolly newWoolly = new Woolly(name, oname, type, length, color);
+            WoollyBears.Add(newWoolly);
+        }
+    }
+}
